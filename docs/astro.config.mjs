80f5978b0ac972217eb1b0706a982e3320180b10
Jukka-Pekka Keisala <jp@jpkeisala.com>
2024-10-01 13:27:52 +0200

    add astro

---
+++ docs/astro.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://flowcourier.github.io',
+	integrations: [
+		starlight({
+			title: 'Flowcourier Support',
+			social: {
+				github: 'https://github.com/flowcourier/Flowcourier.Umbraco.Support',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+				{
+					label: 'Google Analytics for Umbraco',
+					autogenerate: { directory: 'google-analytics-for-umbraco' },
+				},
+			],
+		}),
+	],
+});
