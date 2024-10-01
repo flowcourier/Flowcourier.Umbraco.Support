a1c083c61638905111bd72aedf6729c350f72794
Jukka-Pekka Keisala <jp@jpkeisala.com>
2024-10-01 13:47:27 +0200

    base: '/Flowcourier.Umbraco.Support/'

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://flowcourier.github.io',
+	base: '/Flowcourier.Umbraco.Support/', // Repository name
 	
 	integrations: [
 		starlight({
